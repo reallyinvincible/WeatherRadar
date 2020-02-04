02adfc7ab24106a32a6bd32d0db3514ca0463b4e
reallyinvincible <mailtosparshsri@gmail.com>
2020-02-04 15:53:33 +0530

    added view models

--- app/src/main/java/com/exuberant/weatherradar/ForecastApplication.kt
+++ app/src/main/java/com/exuberant/weatherradar/ForecastApplication.kt
@@ -5,11 +5,15 @@ import com.exuberant.weatherradar.data.db.ForecastDatabase
 import com.exuberant.weatherradar.data.network.*
 import com.exuberant.weatherradar.data.repository.ForecastRepository
 import com.exuberant.weatherradar.data.repository.ForecastRepositoryImpl
+import com.exuberant.weatherradar.ui.weather.current.CurrentWeatherViewModel
+import com.exuberant.weatherradar.ui.weather.current.CurrentWeatherViewModelFactory
+import com.jakewharton.threetenabp.AndroidThreeTen
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class ForecastApplication: Application(), KodeinAware {
@@ -23,6 +27,12 @@ class ForecastApplication: Application(), KodeinAware {
         bind() from singleton { ApixuWeatherApiService(instance()) }
         bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+        bind() from provider { CurrentWeatherViewModelFactory(instance()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
     }
 
 }
--- app/src/main/java/com/exuberant/weatherradar/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/exuberant/weatherradar/ui/weather/current/CurrentWeatherViewModel.kt
@@ -1,7 +1,14 @@
 package com.exuberant.weatherradar.ui.weather.current
 
 import androidx.lifecycle.ViewModel
+import com.exuberant.weatherradar.data.repository.ForecastRepository
+import com.exuberant.weatherradar.internal.lazyDeferred
 
-class CurrentWeatherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CurrentWeatherViewModel(
+    private val forecastRepository: ForecastRepository
+) : ViewModel() {
+
+    val weather by lazyDeferred {
+        forecastRepository.getCurrentWeather()
+    }
 }
